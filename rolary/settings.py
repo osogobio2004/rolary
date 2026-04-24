d6dd8989ce42a3f1dbc1c22eb9a5fe17d3caeed7
osogobio2004 <rominaosogobio2004@gmail.com>
2026-04-23 20:07:18 -0600

    static files

--- rolary/settings.py
+++ rolary/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# ¡Esta es la pieza que faltaba!
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
